c9d9b0ebf42fc42b3b3a99e436ffacf923612c80
Nihir Patel <nihirpatel@ufl.edu>
2019-01-29 13:52:50 -0500

    Update dashboard.module.ts

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -6,11 +6,10 @@ import { RouterModule } from "@angular/router";
 import { dashboardRoutes } from "./dashboard.routes";
 import { ProfileComponent } from "./profile/profile.component";
 import { AuthGuard } from '../guards/auth-guard.service';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [LayoutComponent, HomeComponent, ProfileComponent],
-  imports: [CommonModule, RouterModule.forChild(dashboardRoutes), FontAwesomeModule],
+  imports: [CommonModule, RouterModule.forChild(dashboardRoutes)],
   providers: [AuthGuard]
 })
 export class DashboardModule {}
