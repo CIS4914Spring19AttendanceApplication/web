df44c46fc62fd766b9d62ee02d2ec1cc81bfaea0
Nihir Patel <nihirpatel@ufl.edu>
2019-02-06 13:04:48 -0500

    Update env.config.ts

--- src/app/core/env.config.ts
+++ src/app/core/env.config.ts
@@ -1,4 +1,4 @@
-import { isDevMode } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 const getHost = () => {
   const protocol = window.location.protocol;
@@ -6,10 +6,10 @@ const getHost = () => {
   return `${protocol}//${host}`;
 };
 
+const apiURI = environment.production ? 'https://rollcall-api.herokuapp.com/api' : `http://localhost:8080/api`;
 
-
-const apiURI = !isDevMode() ? 'https://rollcall-api.herokuapp.com/api' : `http://localhost:8080/api`;
 console.log(apiURI);
+
 export const ENV = {
   BASE_URI: getHost(),
   BASE_API: apiURI
