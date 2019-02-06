a1005572107820d82a6363d3e9b75919817c6a9a
Nihir Patel <nihirpatel@ufl.edu>
2019-02-06 12:51:49 -0500

    Try#2

--- src/app/core/env.config.ts
+++ src/app/core/env.config.ts
@@ -1,11 +1,14 @@
-const _isDev = window.location.port.indexOf('4200') < -1;
+import { isDevMode } from '@angular/core';
+
 const getHost = () => {
   const protocol = window.location.protocol;
   const host = window.location.host;
   return `${protocol}//${host}`;
 };
 
-const apiURI = _isDev ? 'https://rollcall-api.herokuapp.com/api' : `http://localhost:8080/api`;
+
+
+const apiURI = !isDevMode ? 'https://rollcall-api.herokuapp.com/api' : `http://localhost:8080/api`;
 console.log(apiURI);
 export const ENV = {
   BASE_URI: getHost(),
