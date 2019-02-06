4a2229848aadc807ea72882770b6990e7bb8603c
Nihir Patel <nihirpatel@ufl.edu>
2019-02-06 12:55:54 -0500

    Update env.config.ts

--- src/app/core/env.config.ts
+++ src/app/core/env.config.ts
@@ -8,7 +8,7 @@ const getHost = () => {
 
 
 
-const apiURI = !isDevMode ? 'https://rollcall-api.herokuapp.com/api' : `http://localhost:8080/api`;
+const apiURI = !isDevMode() ? 'https://rollcall-api.herokuapp.com/api' : `http://localhost:8080/api`;
 console.log(apiURI);
 export const ENV = {
   BASE_URI: getHost(),
