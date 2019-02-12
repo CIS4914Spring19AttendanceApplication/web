37a5b87dbe56bf279dac4aef40f177c10633c81b
Nihir Patel <nihirpatel@ufl.edu>
2019-02-12 09:40:35 -0500

    Org creation and profile editing - no validation for forms yet

--- src/app/dashboard/organization/organization-home/organization-home.component.ts
+++ src/app/dashboard/organization/organization-home/organization-home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from 'src/app/shared-data.service';
 
 @Component({
   selector: 'fmyp-organization-home',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OrganizationHomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(public sharedData: SharedDataService) { }
 
   ngOnInit() {
   }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { SharedDataService } from '../shared-data.service';
+import { Router } from '@angular/router';
+import { UserService } from '../api/user.service';
+
 
 @Component({
   selector: 'fmyp-register',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private sharedData: SharedDataService, private router: Router, private userService:UserService) {
+
+  }
+
+  user = { first_name: null, last_name: null, email: this.sharedData.email, year: 'Choose Year'};
 
   ngOnInit() {
+    if(!this.sharedData.email){
+      this.router.navigate(['/login']);
+    }
+  }
+
+
+  public register() {
+    this.userService.register(this.user).toPromise()
+    .then(result => {
+      console.log(result);
+      this.router.navigate(['/dashboard']);
+    })
+    .catch(error => {
+      console.log(error);
+    });
   }
 
 }
